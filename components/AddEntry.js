e949d48ae84a89a5f8eb75f1ebfd1fcb40f66d1d
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2018-12-24 17:36:45 +0000

    Slider, Stepper, Date and Button

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { Text, View, TouchableOpacity } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import UdaciSlider from './UdaciSlider'
+import UdaciSteppers from './UdaciSteppers'
+import DateHeader from './DateHeader'
+
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component {
 
@@ -42,11 +54,61 @@ export default class AddEntry extends Component {
     }))
   }
 
+  submit = () => {
+
+    const key = timeToString()
+    const entry = this.state
+
+    // Update Redux
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    }))
+
+    // Navigate to home
+
+    // Save to 'DB'
+
+    // Clean local notification
+
+  }
+
+
 
   render() {
+    const metaInfo = getMetricMetaInfo()
+
     return (
       <View>
-        {getMetricMetaInfo('bike').getIcon()}
+
+        <DateHeader date={(new Date()).toLocaleDateString()} />
+        {Object.keys(metaInfo).map((key) => {
+          const { getIcon, type, ...rest } = metaInfo[key]
+          const value = this.state[key]
+
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider'
+                ? <UdaciSlider
+                  value={value}
+                  onChange={(value) => this.increment(key)}
+                  {...rest}
+                />
+                : <UdaciSteppers
+                  value={value}
+                  onIncrement={() => this.increment(key)}
+                  onDecrement={() => this.decrement(key)}
+                  {...rest}
+                />}
+            </View>
+          )
+        })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     )
   }
