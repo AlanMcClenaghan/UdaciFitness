4f33fe9c12a06c0717f64daa335c963e5eaf0c57
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2018-12-29 14:17:12 +0000

    Stack Navigator

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, Platform, StyleSheet } from 'react-native'
-import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
+import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue
+} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
@@ -9,20 +13,19 @@ import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
-import { white, purple } from '../utils/colors'
+import { purple, white } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn({ onPress }) {
   return (
     <TouchableOpacity
-      style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
+      style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
       onPress={onPress}>
       <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   )
 }
-
 class AddEntry extends Component {
-
   state = {
     run: 0,
     bike: 0,
@@ -30,7 +33,6 @@ class AddEntry extends Component {
     sleep: 0,
     eat: 0,
   }
-
   increment = (metric) => {
     const { max, step } = getMetricMetaInfo(metric)
 
@@ -39,69 +41,55 @@ class AddEntry extends Component {
 
       return {
         ...state,
-        [metric]: count > max ? max : count
+        [metric]: count > max ? max : count,
       }
     })
   }
-
   decrement = (metric) => {
     this.setState((state) => {
       const count = state[metric] - getMetricMetaInfo(metric).step
 
       return {
         ...state,
-        [metric]: count < 0 ? 0 : count
+        [metric]: count < 0 ? 0 : count,
       }
     })
   }
-
   slide = (metric, value) => {
     this.setState(() => ({
-      [metric]: value,
+      [metric]: value
     }))
   }
-
   submit = () => {
-
     const key = timeToString()
     const entry = this.state
 
-    // Update Redux
     this.props.dispatch(addEntry({
       [key]: entry
     }))
 
-    this.setState(() => ({
-      run: 0,
-      bike: 0,
-      swim: 0,
-      sleep: 0,
-      eat: 0,
-    }))
+    this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
 
-    // Navigate to home
+    this.toHome()
 
-    // Save to 'DB'
     submitEntry({ key, entry })
 
-    // Clean local notification
-
+    // Clear local notification
   }
-
   reset = () => {
     const key = timeToString()
 
-    // Update Redux
     this.props.dispatch(addEntry({
       [key]: getDailyReminderValue()
     }))
 
-    // Route to Home
+    this.toHome()
 
-    // Update "DB"
     removeEntry(key)
   }
-
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({ key: 'AddEntry' }))
+  }
   render() {
     const metaInfo = getMetricMetaInfo()
 
@@ -112,17 +100,16 @@ class AddEntry extends Component {
             name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
             size={100}
           />
-          <Text>You aleady logged your information for today.</Text>
-          <TextButton onPress={this.reset} style={{ padding: 10 }}>
+          <Text>You already logged your information for today.</Text>
+          <TextButton style={{ padding: 10 }} onPress={this.reset}>
             Reset
           </TextButton>
-        </View >
+        </View>
       )
     }
 
     return (
       <View style={styles.container}>
-
         <DateHeader date={(new Date()).toLocaleDateString()} />
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key]
@@ -134,7 +121,7 @@ class AddEntry extends Component {
               {type === 'slider'
                 ? <UdaciSlider
                   value={value}
-                  onChange={(value) => this.increment(key)}
+                  onChange={(value) => this.slide(key, value)}
                   {...rest}
                 />
                 : <UdaciSteppers
@@ -153,16 +140,15 @@ class AddEntry extends Component {
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: white,
+    backgroundColor: white
   },
   row: {
     flexDirection: 'row',
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   iosSubmitBtn: {
     backgroundColor: purple,
@@ -172,15 +158,16 @@ const styles = StyleSheet.create({
     marginLeft: 40,
     marginRight: 40,
   },
-  androidSubmitBtn: {
+  AndroidSubmitBtn: {
     backgroundColor: purple,
     padding: 10,
     paddingLeft: 30,
     paddingRight: 30,
-    borderRadius: 2,
     height: 45,
+    borderRadius: 2,
     alignSelf: 'flex-end',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   submitBtnText: {
     color: white,
@@ -191,9 +178,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginRight: 30,
     marginLeft: 30,
-  }
+    marginRight: 30,
+  },
 })
 
 function mapStateToProps(state) {
@@ -204,4 +191,6 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(AddEntry) 
+export default connect(
+  mapStateToProps
+)(AddEntry)
